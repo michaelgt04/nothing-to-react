ac1670dbf524ca9317cf3c532da1cc49dbb2be6c
Mike Thomson <michaelgt04@gmail.com>
2018-11-29 03:18:07 +0000

    Clean up query to only get the specific post for page creation

--- src/templates/lesson-template.js
+++ src/templates/lesson-template.js
@@ -4,39 +4,30 @@ import { StaticQuery, graphql } from 'gatsby'
 import Layout from '../components/layout'
 
 const LessonTemplate = props => {
-  return (
-  <StaticQuery
-    query={graphql`
-      {
-        allMarkdownRemark {
-          edges {
-            node {
-              id
-              frontmatter {
-                slug
-              }
-              html
-            }
-          }
-        }
-      }
-    `}
-    render={data => {
-      const post = data.allMarkdownRemark.edges.find(
-        node => node.node.frontmatter.slug === props.pageContext.slug
-      )
+  const { data } = props;
 
-      return(
-        <Layout>
-          <h1>Hoi this is template</h1>
-          <div
-            dangerouslySetInnerHTML={{ __html: post.node.html }}
-          />
-        </Layout>
-      )
-    }}
-  />
+  console.log(data)
+  const lesson = data.markdownRemark.html
+
+  return (
+    <Layout>
+      <h1>Hoi this is template</h1>
+      <div
+        dangerouslySetInnerHTML={{ __html: lesson}}
+      />
+    </Layout>
   )
 }
+
+export const pageQuery = graphql`
+  query LessonByPath($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug }}) {
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`;
 
 export default LessonTemplate;
